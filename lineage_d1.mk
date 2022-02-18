e33e57d560521de60fc8131eb495e51c5a1c39a3
Tim Zimmermann <tim@linux4.de>
2022-02-18 16:25:39 +0100

    d1: Specify device has a S Pen
    Change-Id: I45113a181ab47ec83f856ac7d65563847ce60774

--- lineage_d1.mk
+++ lineage_d1.mk
@@ -24,6 +24,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 ## Inscreen Fingerprint HAL
 TARGET_HAVE_FOD := true
 
+## S Pen
+TARGET_HAVE_SPEN := true
+
 ## Inherit from d1 device
 $(call inherit-product, device/samsung/d1/device.mk)
 
