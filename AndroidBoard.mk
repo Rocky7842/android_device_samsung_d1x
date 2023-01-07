86c5e90aa205dad7adc71da8d09cee24e4440c49
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2023-01-07 17:59:38 +0800

    d1x: Import firmware from N971NKSU2HVG7 and ship them with builds

---
+++ AndroidBoard.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+INSTALLED_RADIOIMAGE_TARGET += \
+    $(DEVICE_PATH)/firmware/models \
+    $(DEVICE_PATH)/firmware/version_SM-N971N \
+    $(DEVICE_PATH)/firmware/cm.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/dqmdbg.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/keystorage.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/modem.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/modem_5g.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/modem_debug.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/modem_debug_5g.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/param.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/sboot.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/uh.bin_SM-N971N \
+    $(DEVICE_PATH)/firmware/up_param.bin_SM-N971N
